d0132f2fa22d9659428a1cc3227ab1da725c2253
engrsyedsaad <syedsaadbintariq@gmail.com>
2026-01-16 17:46:28 +0500

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 
 //CORS Middleware
 app.use(cors({
-    origin: ["http://127.0.0.1:3000", "http://127.0.0.1:3001"],
+    origin: ["http://127.0.0.1:3000", "http://127.0.0.1:3001","https://test2.apmoa.org.pk/"],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true,
 }))
